c1880b06301364c2b4514ddb57191494437e6f43
Biplove Lamichhane <lcbiplove1@gmail.com>
2024-08-20 20:58:03 +1000

    added template support

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
-FLASK_APP=imagecolor
-FLASK_ENV=dev
+FLASK_APP=app
+FLASK_ENV=development
 FLASK_RUN_PORT=8080
--- imagecolor/settings.py
+++
@@ -1,5 +0,0 @@
-#demo/settings.py
-from os import environ
-
-SECRET_KEY = environ.get('SECRET_KEY')
-API_KEY = environ.get('API_KEY')
