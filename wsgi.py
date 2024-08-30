68eb9abbb6ef9fc48cbf00604f74e6aae546f814
Biplove Lamichhane <lcbiplove1@gmail.com>
2024-08-31 09:30:37 +1000

    added waitress to serve instead of gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from app import create_app
+from waitress import serve
 
 main_app = create_app()
 if __name__ == "__main__":
-    main_app.run()
+    serve(main_app, host='0.0.0.0', port=8000, url_scheme='https')
